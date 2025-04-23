fd19706b17ff241d24c6f6e19732317cfd0ababb
Mouhamed Pouye <pouyemouha7@gmail.com>
2025-04-23 12:01:23 +0000

    Change marker icon

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,7 +1,16 @@
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import { Icon } from "leaflet"
 import "leaflet/dist/leaflet.css"
 
 const Map = ({ tractorAds }) => {
+  const markertIcon = new Icon({
+    iconUrl:
+      "https://www.iconpacks.net/icons/2/free-location-pin-icon-2965-thumb.png",
+    iconSize: [35, 35], // size of the icon
+    iconAnchor: [22, 40], // point of the icon which will correspond to marker's location
+    popupAnchor: [-3, -40], // point from which the popup should open relative to the iconAnchor
+  })
+
   return (
     <div className="lg:w-1/2 h-[500px]">
       <MapContainer
@@ -16,7 +25,7 @@ const Map = ({ tractorAds }) => {
         />
         {/* Marqueurs pour chaque annonce */}
         {tractorAds.map(ad => (
-          <Marker position={ad.coordinates}>
+          <Marker position={ad.coordinates} icon={markertIcon}>
             <Popup>{ad.location}</Popup>
           </Marker>
         ))}
